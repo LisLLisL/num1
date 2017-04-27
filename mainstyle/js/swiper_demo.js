3ba33fd4a17cd61f6abeae49e46508d331afb1e1
LisLLisL <371470399@qq.com>
2017-04-27 09:50:18 +0800

    add

---
+++ mainstyle/js/swiper_demo.js
@@ -0,0 +1,27 @@
+$(function(){
+   var mySwiper1 = new Swiper ('.swiper-container1', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: '.swiper-pagination1',
+  })
+
+  var mySwiper2 = new Swiper ('.swiper-container2', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: '.swiper-pagination2',
+
+  })  
+  var mySwiper3 = new Swiper ('.swiper-container3', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: '.swiper-pagination3',
+
+  })  
+  var mySwiper4 = new Swiper ('.swiper-container4', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: '.swiper-pagination4',
+
+  }) 
+})
+       
